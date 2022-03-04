a507deb6546e08f4590907869a39ad9ce30f3657
Kevin Voß <kevin.voss@micromerce.com>
2022-03-04 19:48:10 +0100

    .

--- config/mongoDB.js
+++ config/mongoDB.js
@@ -1,8 +1,6 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI =
-  process.env.MONGODB_URI ||
-  "mongodb+srv://AvaPLaIN:EXvsKzxbTozRhJ4x@cluster0.m0luj.mongodb.net/next_weebflix?retryWrites=true&w=majority";
+const MONGODB_URI = process.env.MONGODB_URI;
 
 if (!MONGODB_URI) {
   throw new Error(
