ca07a52cd00458f4efc8fee5d0a3c59b49c576cc
Kevin Voß <kevin.voss@micromerce.com>
2022-03-07 11:19:11 +0100

    .

--- pages/anime/[id].jsx
+++ pages/anime/[id].jsx
@@ -76,7 +76,7 @@ const AnimeId = ({ anime, groupAnimes }) => {
           <Seasons animes={groupAnimes} />
           <Episodes episodes={anime.episodes} />
         </div>
-        <video controls />
+        <video controls height="350px" width="600px" />
       </LayoutContainer>
     </div>
   );
